c5d214297ac03c9c4d1f66221806b4654b7fc295
r-i-c-h-a <richa5june99@gmail.com>
2020-04-27 03:10:20 +0530

    Added crash apis

--- orchestrator.py
+++ orchestrator.py
@@ -4,6 +4,7 @@ import json
 from flask import Response
 from bson.json_util import dumps
 import pika
+import os
 
 app = Flask(__name__)
 
@@ -18,6 +19,11 @@ channel_w.queue_declare(queue="writeq")
 resp_connection = pika.BlockingConnection(pika.ConnectionParameters(host='rmq'))
 resp_channel = resp_connection.channel()
 resp_channel.queue_declare(queue="responseq")
+
+path = os.path.basename(os.getcwd())
+ismaster = lambda a: bool(a.startswith(path+"_master"))
+isslave = lambda a: bool(a.startswith(path+"_slave"))
+isworker = lambda a: bool(a.startswith(path+"_worker"))
 
 class Callback(object):
     def __init__(self,body):
@@ -54,19 +60,47 @@ def write():
     return "{}"
 
 
-@app.route('/api/v1/crash/master')
+@app.route('/api/v1/crash/master',methods=["POST"])
 def kill_master():
-    pass
+    workers = []
+    output = [i.strip('\n') for i in os.popen('sudo docker ps --format "{{.Names}}"').readlines()]
+    for i in output:
+        if ismaster(i) or isslave(i) or isworker(i):
+            workers.append(i)
+    pids = [int(os.popen('sudo docker inspect --format="{{.State.Pid}}" '+i).read().strip('\n')) for i in workers]
+    to_kill = workers[pids.index(min(pids))]
+    flag = os.system("sudo docker kill "+to_kill)
+    if flag:
+        return "{}"
+    #pass
 
 
-@app.route('/api/v1/crash/slave')
+@app.route('/api/v1/crash/slave',methods=["POST"])
 def kill_highest_slave():
-    pass
+    workers = []
+    output = [i.strip('\n') for i in os.popen('sudo docker ps --format "{{.Names}}"').readlines()]
+    for i in output:
+        if ismaster(i) or isslave(i) or isworker(i):
+            workers.append(i)
+    pids = [int(os.popen('sudo docker inspect --format="{{.State.Pid}}" '+i).read().strip('\n')) for i in workers]
+    to_kill = workers[pids.index(max(pids))]
+    flag = os.system("sudo docker kill "+to_kill)
+    if flag:
+        return "{}"
+    #pass
 
 
-@app.route('/api/v1/worker/list')
+@app.route('/api/v1/worker/list',methods=["GET"])
 def get_sorted_workers_pid():
-    pass
+    workers = []
+    output = [i.strip('\n') for i in os.popen('sudo docker ps --format "{{.Names}}"').readlines()]
+    for i in output:
+        if ismaster(i) or isslave(i) or isworker(i):
+            workers.append(i)
+    pids = [int(os.popen('sudo docker inspect --format="{{.State.Pid}}" '+i).read().strip('\n')) for i in workers]
+    response = sorted(pids)
+    return(json.dumps(response))
+    #pass
 
 
 
